d81bbd5d81d0e5de9a7d20ef7459f2ee8e23b2ff
Joeb3219 <joseph.a.boyle@rutgers.edu>
2017-09-26 15:25:06 -0400

    Initial commit

---
+++ malloc/malloc.c
@@ -0,0 +1,16 @@
+/*
+ * Malloc implementations
+ * September 26, 2017
+ * Joseph A. Boyle
+ * Rutgers, The State University of New Jersey
+ */
+
+/*
+ * All of these implementations are written for architectures assuming integers are >= 4 bytes.
+ */
+
+#define MALLOC_INITIAL_REQUEST_SIZE 4096
+
+void* mymalloc(int size){
+	
+}
